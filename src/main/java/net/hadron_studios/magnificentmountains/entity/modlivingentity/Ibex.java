4aa1fe1d1734ed65fba9810cb664a2a5b4835950
AzureModding <rhodrifbjohnson@hotmail.com>
2022-06-06 20:12:39 -0400

    Added Ibex file for later, moving on to make the Snow Leopards breedable

---
+++ src/main/java/net/hadron_studios/magnificentmountains/entity/modlivingentity/Ibex.java
@@ -0,0 +1,34 @@
+package net.hadron_studios.magnificentmountains.entity.modlivingentity;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+//public class Ibex extends Animal {
+    //protected Ibex(EntityType<? extends Animal> entityType, Level level) {
+        //super(entityType, level);
+   // }
+
+  //  @Override
+   // protected void registerGoals() {
+       // this.goalSelector.addGoal(0, new FloatGoal(this));
+       // this.goalSelector.addGoal(1, new PanicGoal(this, 2.0D));
+      //  this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
+      //  this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(Items.BEETROOT), false));
+       // this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+      //  this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
+      //  this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+    //}
+
+
+   // public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
+   //     return null;
+   // }
+//}
